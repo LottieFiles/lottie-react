71070bb5736ba8c4110c26de622acabca313b6e5
Karam <karam7898@gmail.com>
2020-05-23 00:56:04 +0800

    added story book for dev

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        // Optional
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+};
