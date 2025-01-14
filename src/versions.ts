a570be39e57d31f775f0ecac95bc7c3936812226
Abdelrahman Ashraf <a.theashraf@gmail.com>
2025-01-14 16:54:51 +0700

    chore: run lint on PRs  (#171)
    * chore: update GitHub Actions workflow to include 'master' branch for pull requests

--- src/versions.ts
+++ src/versions.ts
@@ -1,2 +1,2 @@
-export const REACT_LOTTIE_PLAYER_VERSION = "3.5.4"; 
-export const LOTTIE_WEB_VERSION = "^5.12.2";
+export const REACT_LOTTIE_PLAYER_VERSION = '3.5.4';
+export const LOTTIE_WEB_VERSION = '^5.12.2';
